bcd3761c55e1d03ba84a3168b3e9791427f11b3c
Davide <davide.0plus1@protonmail.com>
2021-02-01 10:06:01 +1100

    Correctly reference shelves in books

--- Backend/data-seeds/1-shelves/shelves.js
+++ Backend/data-seeds/1-shelves/shelves.js
@@ -1,19 +1,23 @@
 const faker = require('faker');
+const { getObjectId } = require('mongo-seeding');
 
 const shelves = [
   {
+    _id: getObjectId('fiction'),
     name: 'Fiction',
     slug: 'fiction',
     createdAt: faker.date.recent(),
     updatedAt: faker.date.recent(),
   },
   {
+    _id: getObjectId('non-fiction'),
     name: 'Non fiction',
     slug: 'non-fiction',
     createdAt: faker.date.recent(),
     updatedAt: faker.date.recent(),
   },
   {
+    _id: getObjectId('poetry'),
     name: 'Poetry',
     slug: 'poetry',
     createdAt: faker.date.recent(),
--- Backend/data-seeds/2-books/books.js
+++ Backend/data-seeds/2-books/books.js
@@ -2,10 +2,10 @@ const faker = require('faker');
 const { getObjectId } = require('mongo-seeding');
 
 const books = [];
-const shelves = ['Fiction', 'Non Fiction', 'Poetry'];
+const shelves = ['fiction', 'non-fiction', 'poetry'];
 for (let i = 0; i < 100; i += 1) {
   books.push({
-    categoryId: getObjectId(shelves[Math.floor(Math.random() * shelves.length)]),
+    shelfId: getObjectId(shelves[Math.floor(Math.random() * shelves.length)]),
     title: faker.commerce.productName(),
     isbn: faker.random.alphaNumeric(10),
     createdAt: faker.date.recent(),
